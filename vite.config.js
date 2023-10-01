818034084f256251e8c5cae7eba74bac777ece71
matthieuGravy <matthieugravy@gmail.com>
2023-10-01 12:06:28 +0200

    test5

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 // https://vitejs.fr/guide/static-deploy.html
 export default defineConfig({
-  base: "./",
+  base: "./dist/",
 });
